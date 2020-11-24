6e5ee64f57e86cfce2acb92346076f43b6dd4462
Peerasak Longchit <peerasak.lo@baac.or.th>
2020-11-24 14:14:53 +0700

    Add project files.

---
+++ ScaffoldingDesignTimeServices.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.EntityFrameworkCore.Scaffolding.Internal;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace POCO
+{
+    public class ScaffoldingDesignTimeServices : IDesignTimeServices
+    {
+        public void ConfigureDesignTimeServices(IServiceCollection services)
+        {
+            services.AddSingleton<IScaffoldingModelFactory, CustomModelFactory>();
+        }
+    }
+}
